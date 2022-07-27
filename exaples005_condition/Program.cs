b89a2ea207a979e2f809ab6b42dfe91f29b890e6
Ismoil <programmer7575@gmail.com>
2022-07-27 10:59:32 +0500

    Hello Obito!

---
+++ exaples005_condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "обито")
+{
+    Console.WriteLine("Ура, это же Обито");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
